13e9c13fbfee7926528887de08ba51f76a35f49d
Emmanuel <ogoma.emmanuel@gmail.com>
2018-07-22 17:40:48 +0300

    Done with video 10, lesson learnt, testing validation rules

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -20,6 +20,10 @@ class RepliesController extends Controller
     public function store($channelid,Thread $thread)
     {
 
+        $this->validate(request(),[
+            'body'=>'required'
+        ]);
+
         $thread->addReply([
             'body'=>request('body'),
             'user_id'=>auth()->id(),
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -32,12 +32,41 @@ class CreateThreadsTest extends TestCase
 
     function an_authenticated_user_can_create_new_threads(){
         $this->signIn();
-        $thread=create('App\Thread');
-        $this->post('/threads', $thread->toArray());
-
-        //dd($thread->path());
-        $this->get($thread->path())
+        $thread=make('App\Thread');
+       $response= $this->post('/threads', $thread->toArray());
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
+    }
+
+    /** @test */
+    function a_thread_requires_a_title(){
+        $this->publishThread(['title'=>null])
+            ->assertSessionHasErrors('title');
+
+    }
+    /** @test */
+    function a_thread_requires_a_body(){
+        $this->publishThread(['body'=>null])
+            ->assertSessionHasErrors('body');
+
+    }
+
+    /** @test */
+    function a_thread_requires_a_valid_channel(){
+        factory("App\Channel",2)->create();
+
+        $this->publishThread(['channel_id'=>null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id'=>999])
+            ->assertSessionHasErrors('channel_id');
+
+    }
+
+    function publishThread($overrides){
+        $this->withExceptionHandling()->signIn();
+        $thread=make("App\Thread",$overrides);
+        return $this->post('threads',$thread->toArray());
     }
 }
