03016c3d626a9b68c920f0b6120277c30923f866
Emmanuel <ogoma.emmanuel@gmail.com>
2018-08-14 21:45:59 +0300

    Started and finished video 18, a user can favorite a reply, lesson learnt, polymophic relationships

---
+++ tests/Feature/FavoritesTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+
+class FavoritesTest extends TestCase
+{
+    Use DatabaseMigrations;
+
+
+    /** @test */
+    function guest_can_not_favorite_anything(){
+        $this->withExceptionHandling()
+
+            ->post("replies/1/favorites")
+
+            ->assertRedirect('/login');
+
+    }
+
+
+    /** @test */
+    function an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply');
+
+        $this->post('replies/'.$reply->id."/favorites");
+
+        $this->assertCount(1,$reply->favorites);
+    }
+
+    /** @test */
+    function an_authenticated_user_may_only_favorite_a_reply_once()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply');
+
+        $this->post('replies/'.$reply->id."/favorites");
+
+        $this->post('replies/'.$reply->id."/favorites");
+
+        $this->assertCount(1,$reply->favorites);
+    }
+}
