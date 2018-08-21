7010255c49486718b3a388739645dfdf6ad1b28e
Emmanuel Ogoma <eogoma@cytonn.com>
2018-08-21 09:33:51 +0300

    Deleting using psedo foreach

--- app/Thread.php
+++ app/Thread.php
@@ -23,11 +23,8 @@ class Thread extends Model
         });
 
         static::deleting(function ($thread) {
-            $thread->replies->each(function ($reply){
-                $reply->delete();
-            });
+            $thread->replies->each();
         });
-
     }
 
     public function path()
