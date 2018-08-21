5881d1135db46a6e6ec8ccd6d075cfca3d003851
Emmanuel Ogoma <eogoma@cytonn.com>
2018-08-21 09:43:58 +0300

    Testting database records using count method

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use Illuminate\Auth\AuthenticationException;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -105,17 +106,7 @@ class CreateThreadsTest extends TestCase
 
         $this->assertDatabaseMissing('threads', ['id'=>$thread->id]);
         $this->assertDatabaseMissing('replies', ['id'=>$reply->id]);
-        $this->assertDatabaseMissing('activities',
-            [
-                'subject_id'=>$thread->id,
-                'subject_type'=>get_class($thread)
-            ]);
-
-        $this->assertDatabaseMissing('activities',
-            [
-                'subject_id'=>$reply->id,
-                'subject_type'=>get_class($reply)
-            ]);
+        $this->assertEquals(0,Activity::count());
 
     }
 
