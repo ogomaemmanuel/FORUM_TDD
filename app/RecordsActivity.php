1b6079877b837d08d430c078ba765a6a6fe8e0a6
Emmanuel Ogoma <eogoma@cytonn.com>
2018-08-21 09:31:59 +0300

    using foreach

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -15,16 +15,23 @@ trait RecordsActivity
     protected static function bootRecordsActivity()
     {
 
-        if(auth()->guest()) return ;
+        if (auth()->guest()) {
+            return;
+        }
 
-        foreach (static::getActivitiesToRecord() as $event){
+        foreach (static::getActivitiesToRecord() as $event) {
 
-            static::created(function ($model) use ($event ){
+            static::created(function ($model) use ($event) {
 
                 $model->recordActivity($event);
             });
 
         }
+
+        static::deleting(function ($model) {
+
+            $model->activity()->delete();
+        });
 
     }
 
@@ -43,8 +50,9 @@ trait RecordsActivity
     }
 
 
-    protected function activity(){
-      return  $this->morphMany("App\Activity","subject");
+    protected function activity()
+    {
+        return $this->morphMany("App\Activity", "subject");
     }
 
     protected function getActivityType($event)
